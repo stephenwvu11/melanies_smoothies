355294c0e5c53ad7bf2ca7d06e4e40b2b3431120
stephenwvu11 <boalosb@upmc.edu>
2026-06-23 12:55:09 -0400

    Fix variable assignment for Pandas DataFrame conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options") .select(col('FRUI
 # st.stop()         
 
 # Convert the Snowpark Datafram to a Pandas Dataframe so we can use the LOC function
-pd=df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
                                                                        
